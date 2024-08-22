d02b09b15564a504214c678d4a2618b107c321b8
anatxzier <anatxzier@gmail.com>
2024-08-22 16:19:13 -0300

    estilização: styleNovaTurma.css e styleLogin.css
    Co-authored-by: mariadudasn <mariadudasn@users.noreply.github.com>

---
+++ RPA/static/js/animationNovaTurma.js
@@ -0,0 +1,13 @@
+
+    // document.addEventListener("DOMContentLoaded", function() {
+    //     const scrollButton = document.querySelector('.scroll-to-bottom-btn');
+    //     const formContainer = document.querySelector('.nova-turma-form');
+        
+    //     scrollButton.addEventListener('click', function() {
+    //         formContainer.scrollTo({
+    //             top: formContainer.scrollHeight,
+    //             behavior: 'smooth'
+    //         });
+    //     });
+    // });
+
