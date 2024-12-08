f9cccd9db4e579f5d33eeeffe3e403c1483ff7ec
Letícia Paiva <133794677+lerpaiva@users.noreply.github.com>
2024-12-08 20:55:38 -0300

    Comentários urls.py

--- RPA/RPA/urls.py
+++ RPA/RPA/urls.py
@@ -3,9 +3,10 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
+# Define a lista de URLs do projeto
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('geenfy.urls')),
-    path('api/', include('apiGeenfy.urls'))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls), # URL para acessar o painel administrativo do Django.
+    path('', include('geenfy.urls')), # Inclui as URLs definidas no aplicativo 'geenfy' para a rota principal.
+    path('api/', include('apiGeenfy.urls')) # Inclui as URLs do aplicativo 'apiGeenfy' na rota 'api/'.
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # Adiciona suporte para servir arquivos estáticos e de mídia durante o desenvolvimento.
 
