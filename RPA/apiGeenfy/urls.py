ac48699342ab4754f4f31f222adff9e67dc1fd07
Letícia Paiva <133794677+lerpaiva@users.noreply.github.com>
2024-12-08 21:23:28 -0300

    Comentários urls.py

--- RPA/apiGeenfy/urls.py
+++ RPA/apiGeenfy/urls.py
@@ -1,12 +1,13 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import In_progress_fileViewSet, Finished_FileViewSet, ErrorLogViewSet
+from django.urls import path, include # Importa as funções para definir e incluir URLs.
+from rest_framework.routers import DefaultRouter # Importa o roteador padrão do Django REST Framework.
+from .views import In_progress_fileViewSet, Finished_FileViewSet, ErrorLogViewSet # Importa os ViewSets que definem a lógica das operações para os endpoints.
 
-router = DefaultRouter()
-router.register(r'documentos', In_progress_fileViewSet)
-router.register(r'concluido', Finished_FileViewSet)
-router.register(r'erro', ErrorLogViewSet)
+router = DefaultRouter() # Cria uma instância do roteador padrão
+router.register(r'documentos', In_progress_fileViewSet) # Registra o ViewSet 'In_progress_fileViewSet' com a rota base 'documentos'.
+router.register(r'concluido', Finished_FileViewSet) # Registra o ViewSet 'Finished_FileViewSet' com a rota base 'concluido'.
+router.register(r'erro', ErrorLogViewSet) # Registra o ViewSet 'ErrorLogViewSet' com a rota base 'erro'.
 
+# Define as URLs do aplicativo usando o roteador
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)), # Inclui todas as rotas registradas no roteador como parte das URLs do aplicativo.
 ]
