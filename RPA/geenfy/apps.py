3f320a71ca517cec500b5b71bd59729ce617dd88
Letícia Paiva <133794677+lerpaiva@users.noreply.github.com>
2024-12-08 21:37:21 -0300

    Comentários apps.py

--- RPA/geenfy/apps.py
+++ RPA/geenfy/apps.py
@@ -1,9 +1,10 @@
-from django.apps import AppConfig
+from django.apps import AppConfig # Importa a classe AppConfig para configurar o aplicativo no Django.
 
-
+# Configuração do aplicativo 'geenfy'
 class GeenfyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'geenfy'
-    
+    default_auto_field = 'django.db.models.BigAutoField' # Define o tipo de campo de chave primária para os modelos.
+    name = 'geenfy' # Define o nome do aplicativo, que deve coincidir com o diretório do aplicativo.
+
+    # Método 'ready' é chamado quando o aplicativo é carregado
     def ready(self):
-        import geenfy.signals  
+        import geenfy.signals # Importa sinais (signals) que são utilizados para adicionar lógica extra ou reagir a eventos específicos.
