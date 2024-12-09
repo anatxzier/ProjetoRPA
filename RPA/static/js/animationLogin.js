409b8dc2baad8ded6eae64ed9835502e6078200b
Letícia Paiva <133794677+lerpaiva@users.noreply.github.com>
2024-12-09 11:29:45 -0300

    Comentários animationLogin.js

--- RPA/static/js/animationLogin.js
+++ RPA/static/js/animationLogin.js
@@ -1,13 +1,16 @@
+// Define um temporizador para esconder a mensagem de alerta após 2.5 segundos
 setTimeout(function () {
     var alertContainer = document.getElementById('alert-containerLogin');
     if (alertContainer) {
         alertContainer.style.display = 'none';
     }
-}, 2500);
+}, 2500); // Tempo de espera de 2.5 segundos (2500 milissegundos)
 
+// Seleciona o botão de mostrar/esconder senha e o campo de senha do login
 const togglePasswordLogin = document.querySelector('#togglePasswordLogin');
 const passwordFieldLogin = document.querySelector('#id_passwordLogin');
 
+// Adiciona um evento de clique ao botão de mostrar/esconder senha
 togglePasswordLogin.addEventListener('click', function () {
     // Verifica o tipo atual do campo de senha
     const type = passwordFieldLogin.getAttribute('type') === 'password' ? 'text' : 'password';
