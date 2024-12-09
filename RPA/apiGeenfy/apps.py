da49c87656940f543c451bfb56a80a41a5efa247
Letícia Paiva <133794677+lerpaiva@users.noreply.github.com>
2024-12-08 21:07:16 -0300

    Comentários apps.py

--- RPA/apiGeenfy/apps.py
+++ RPA/apiGeenfy/apps.py
@@ -1,6 +1,6 @@
-from django.apps import AppConfig
+from django.apps import AppConfig # Importa a classe base para configuração de aplicativos no Django.
 
 
-class ApigeenfyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apiGeenfy'
+class ApigeenfyConfig(AppConfig): # Classe de configuração para o aplicativo 'apiGeenfy'.
+    default_auto_field = 'django.db.models.BigAutoField' # Define o tipo padrão de chave primária para os modelos do aplicativo.
+    name = 'apiGeenfy' # Nome do aplicativo que será usado nas configurações do projeto.
