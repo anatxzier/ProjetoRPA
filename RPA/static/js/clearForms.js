4f87ec77bc782e61473bb3f88aa37b0d56635307
Letícia Paiva <133794677+lerpaiva@users.noreply.github.com>
2024-12-09 11:32:05 -0300

    Comentários clearForms.js

--- RPA/static/js/clearForms.js
+++ RPA/static/js/clearForms.js
@@ -1,4 +1,6 @@
+// Adiciona um ouvinte de evento para o evento "beforeunload" (antes da página ser descarregada)
 window.addEventListener("beforeunload", function() {
+    // Reseta o formulário com o ID "FormsCadastro" antes da página ser descarregada
     document.getElementById("FormsCadastro").reset();
 });
 
