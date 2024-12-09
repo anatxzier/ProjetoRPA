9d6dcc1c4e4f527f5e6ae68d36dcda6cffa10e54
Letícia Paiva <133794677+lerpaiva@users.noreply.github.com>
2024-12-08 21:35:27 -0300

    Comentários admin.py

--- RPA/geenfy/admin.py
+++ RPA/geenfy/admin.py
@@ -1,6 +1,7 @@
-from django.contrib import admin
-from .models import Homepage, Login, NovaTurma, Cadastro, Storage, Processo, Funcionario, Usuario, In_progress_file, Finished_file, Perfil, Cadastro_Info,PerfilEditar, ErrorLog
+from django.contrib import admin # Importa o módulo para registrar modelos no painel administrativo do Django.
+from .models import Homepage, Login, NovaTurma, Cadastro, Storage, Processo, Funcionario, Usuario, In_progress_file, Finished_file, Perfil, Cadastro_Info,PerfilEditar, ErrorLog # Importa os modelos do aplicativo para que possam ser registrados no painel admin.
 
+# Registra os modelos no Django admin para que possam ser manipulados através do painel administrativo.
 admin.site.register(Homepage)
 admin.site.register(Login)
 admin.site.register(NovaTurma)
