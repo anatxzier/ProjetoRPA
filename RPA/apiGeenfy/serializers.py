7c10e813793951924c8285b15b039874231e9634
Letícia Paiva <133794677+lerpaiva@users.noreply.github.com>
2024-12-08 21:14:15 -0300

    Comentários serializers.py

--- RPA/apiGeenfy/serializers.py
+++ RPA/apiGeenfy/serializers.py
@@ -1,16 +1,20 @@
-from rest_framework import serializers
-from geenfy.models import  In_progress_file, Finished_file, ErrorLog
+from rest_framework import serializers # Importa o módulo serializers para criar serializers no Django REST Framework.
+from geenfy.models import  In_progress_file, Finished_file, ErrorLog # Importa os modelos que serão serializados.
 
+# Serializer para o modelo In_progress_file
 class In_progress_fileSerializer(serializers.ModelSerializer):
     class Meta:
         model = In_progress_file
-        fields = ['id', 'turma', 'arquivo_inprogress', 'status', 'upload_time']
+        fields = ['id', 'turma', 'arquivo_inprogress', 'status', 'upload_time'] # Campos que serão incluídos na serialização.
 
+# Serializer para o modelo Finished_file
 class Finished_fileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Finished_file
-        fields = ['id', 'turma', 'arquivo_fineshed', 'upload_time']
+        fields = ['id', 'turma', 'arquivo_fineshed', 'upload_time'] # Campos que serão incluídos na serialização.
+        
+# Serializer para o modelo ErrorLog        
 class ErrorLogSerializer(serializers.ModelSerializer):
     class Meta:
         model = ErrorLog
-        fields = ['id', 'mensagem', 'data_criacao']
+        fields = ['id', 'mensagem', 'data_criacao'] # Campos que serão incluídos na serialização.
