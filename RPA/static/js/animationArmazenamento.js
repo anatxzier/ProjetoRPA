cb86101c55873237a464f3bfb90cc4aabf15bd95
Letícia Paiva <133794677+lerpaiva@users.noreply.github.com>
2024-12-09 11:23:20 -0300

    Comentários animationArmazenamento.js

--- RPA/static/js/animationArmazenamento.js
+++ RPA/static/js/animationArmazenamento.js
@@ -1,68 +1,27 @@
-// // Obter os elementos necessários
-// var modalDelete = document.getElementById("modalDelete");
-// var btnDelete = document.getElementById("buttonModalDelete");
-// var modalRestore = document.getElementById("modalRestore");
-// var btnRestore = document.getElementById("buttonModalRestore");
-
-// var spanClose = document.getElementsByClassName("closeDeleteRestore");
-// var buttonClose = document.getElementsByClassName("voltarDeleteRestore");
-
-// // Quando o botão de deletar for clicado, mostra a modal de deletar
-// btnDelete.onclick = function(event) {
-//     event.preventDefault(); // Previne o envio do formulário
-//     modalDelete.style.display = "block";
-// }
-
-// // Quando o botão de restaurar for clicado, mostra a modal de restaurar
-// btnRestore.onclick = function(event) {
-//     event.preventDefault(); // Previne o envio do formulário
-//     modalRestore.style.display = "block";
-// }
-
-// // Quando o usuário clicar no "x" da modal de deletar ou restaurar, esconde a modal
-// for (let i = 0; i < spanClose.length; i++) {
-//     spanClose[i].onclick = function() {
-//         modalDelete.style.display = "none";
-//         modalRestore.style.display = "none";
-//     }
-// }
-
-// // Quando clicar em "voltar" nas modais, elas fecham
-// for (let i = 0; i < buttonClose.length; i++) {
-//     buttonClose[i].onclick = function() {
-//         modalDelete.style.display = "none";
-//         modalRestore.style.display = "none";
-//     }
-// }
-
-// // Quando o usuário clicar fora da modal, esconde a modal
-// window.onclick = function(event) {
-//     if (event.target == modalDelete) {
-//         modalDelete.style.display = "none";
-//     }
-//     if (event.target == modalRestore) {
-//         modalRestore.style.display = "none";
-//     }
-// }
-
 
 function openModal(modalId) {
+    // Obtém o modal pelo ID fornecido
     var modal = document.getElementById(modalId);
     if (modal) {
+        // Se o modal existir, exibe ele definindo o display como 'block'
         modal.style.display = 'block';
     }
   }
   
   function closeModal(modalId) {
+    // Obtém o modal pelo ID fornecido
     var modal = document.getElementById(modalId);
     if (modal) {
+        // Se o modal existir, oculta ele definindo o display como 'none'
         modal.style.display = 'none';
     }
   }
   
   // Fechar o modal ao clicar fora dele
   window.onclick = function(event) {
+    // Verifica se o clique foi fora do conteúdo do modal (clicando na área de fundo)
     if (event.target.classList.contains('modal')) {
+        // Se o clique foi fora, chama a função closeModal passando o ID do modal (event.target.id)
         closeModal(event.target.id);
     }
   }
